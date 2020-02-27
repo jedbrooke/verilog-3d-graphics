f6ef605684c8d85720cc38a54681df6f0f6f60f7
Jasper Edbrooke <jasperredrock@gmail.com>
2020-02-27 00:27:50 -0800

    update generator

--- generators/generate_matmul.py
+++ generators/generate_matmul.py
@@ -4,14 +4,32 @@ size1,size2 = sys.argv[1],sys.argv[2]
 bit_width = int(sys.argv[3])
 mat1 = [int(s) for s in size1.split('x')]
 mat2 = [int(s) for s in size2.split('x')]
+fp = False
+if "-f" in sys.argv:
+    fp = True
 
 if mat1[1] != mat2[0]:
     print("Error: Matrix size mismatch")
     sys.exit(1)
 
+name = "matmul_{}by{}".format(size1,size2)
+print("//Module for Calculating M = A*B")
+print("//A is a {} matrix, B is a {} matrix, M is a {} matrix".format(size1,size2,"{}x{}".format(mat1[0],mat2[1])))
+print("module {}(A,B,M);".format(name))
+print("\t//input and outputs")
 m_size_1d = (mat1[0] * mat2[1] * bit_width  - 1)
 mat1_size_1d = (mat1[0] * mat1[1] * bit_width  - 1)
 mat2_size_1d = (mat2[0] * mat2[1] * bit_width - 1)
+print("\t//A is {} bits, for {}*{}={} elements, each of which is {} bits wide".format(mat1_size_1d,mat1[0],mat1[1],mat1[0]*mat1[1],bit_width))
+print("\tinput wire [{}:0] A;".format(mat1_size_1d))
+print("\t//B is {} bits, for {}*{}={} elements, each of which is {} bits wide".format(mat2_size_1d,mat2[0],mat2[1],mat2[0]*mat2[1],bit_width))
+print("\tinput wire [{}:0] B;".format(mat2_size_1d))
+print("\t//M is {} bits, for {}*{}={} elements, each of which is {} bits wide".format(m_size_1d,mat1[0],mat2[1],mat1[0]*mat2[1],bit_width))
+print("\toutput wire [{}:0] B;".format(m_size_1d))
+
+
+
+
 assignments = []
 
 for i in range(mat1[0]):
@@ -25,9 +43,12 @@ for i in range(mat1[0]):
             sums.append("(A[{}:{}] * B[{}:{}])".format(amsb,alsb,bmsb,blsb))
         mlsb = m_size_1d - ((i*mat1[0] + j + 1) * bit_width - 1)
         mmsb = m_size_1d - (i*mat1[0] + j) * bit_width
-        assignments.append("assign M[{}:{}] = ".format(mmsb,mlsb) + " + ".join(sums) + ";")
-
+        assignments.append("\tassign M[{}:{}] = ".format(mmsb,mlsb) + " + ".join(sums) + ";")
 print(*assignments,sep="\n")
+print("endmodule")
+
+
+
 
 def print_usage():
     print("Usage: python3 generate matmul N1xN2 N2xN3 bit_width")
