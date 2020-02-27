f3595c8d800b654544ef436bd6cb7837148f31dd
Jasper Edbrooke <jasperredrock@gmail.com>
2020-02-26 22:18:32 -0800

    update generators

--- generators/generate_matmul.py
+++ generators/generate_matmul.py
@@ -9,6 +9,7 @@ if mat1[1] != mat2[0]:
     print("Error: Matrix size mismatch")
     sys.exit(1)
 
+size_1d = (mat1[0] * mat2[1] * bit_width  - 1)
 assignments = []
 
 for i in range(mat1[0]):
@@ -19,9 +20,12 @@ for i in range(mat1[0]):
             alsb = (i*mat1[0] + k) * bit_width
             bmsb = (k*mat2[0] + j + 1) * bit_width - 1
             blsb = (k*mat2[0] + j) * bit_width
-            sums.append("8'b (A[{}:{}] * B[{}:{}])".format(amsb,alsb,bmsb,blsb))
-        assignments.append(" + ".join(sums))
-print("assign M = {"," , ".join(assignments),"};",sep="")
+            sums.append("(A[{}:{}] * B[{}:{}])".format(amsb,alsb,bmsb,blsb))
+        mlsb = size_1d - ((i*mat1[0] + j + 1) * bit_width - 1)
+        mmsb = size_1d - (i*mat1[0] + j) * bit_width
+        assignments.append("assign M[{}:{}] = ".format(mmsb,mlsb) + " + ".join(sums) + ";")
+
+print(*assignments,sep="\n")
 
 def print_usage():
     print("Usage: python3 generate matmul N1xN2 N2xN3 bit_width")
