9a727baedc1023fee23721bd682fd12c8df2b2d4
Jasper Edbrooke <jasperredrock@gmail.com>
2021-02-03 12:41:15 -0800

    add basic test of ray intersection

--- ray_test.cpp
+++ ray_test.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <string>
 class Vector3D
 {
 private:
@@ -7,6 +13,7 @@ public:
     Vector3D(float _x, float _y, float _z);
     Vector3D crossProduct(const Vector3D& v);
     float dotProduct(const Vector3D& v);
+    std::string toString();
     ~Vector3D();
 };
 
@@ -50,18 +57,119 @@ float Vector3D::dotProduct(const Vector3D& a) {
     return (x * a.x) + (y * a.y) + (z * a.z);
 }
 
+std::string Vector3D::toString() {
+    std::stringstream ss;
+    ss << '(' << x << ',' << y << ',' << z << ')';
+    return ss.str();
+}
+
 Vector3D::~Vector3D(){}
 
-struct Triangle
+class Triangle
 {
+public:
     Vector3D vertex0, vertex1, vertex2;
+    Triangle();
+    Triangle(Vector3D v0, Vector3D v1, Vector3D v2);
 };
-typedef struct Triangle Triangle;
+
+Triangle::Triangle(){}
+Triangle::Triangle(Vector3D v0, Vector3D v1, Vector3D v2) {
+    vertex0 = v0;
+    vertex1 = v1;
+    vertex2 = v2;
+}
 
 
 
+Triangle* quadToTri(Vector3D a, Vector3D b, Vector3D c, Vector3D d){
+    Triangle* tri_pair = (Triangle*) malloc(sizeof(Triangle) * 2);
+    tri_pair[0] = Triangle(a,b,c);
+    tri_pair[1] = Triangle(a,d,c);
+    return tri_pair;
+}
 
-bool RayIntersectsTriangle(Vector3D rayOrigin, 
+std::vector<Triangle> readOfftoTris(std::string path) {
+    std::string line;
+    std::ifstream file(path.c_str());
+    std::vector<std::string> lines;
+    if( file.is_open()) {
+        while( getline(file,line) ) {
+            lines.push_back(line);
+        }
+        file.close();
+    } else {
+        std::cerr << "Error reading file" << std::endl;
+    }
+
+    if(lines[0].compare("OFF") != 0) {
+        std::cerr << "Error: \"" << path << "\" is not a valid OFF file" << std::endl;
+    }
+    std::stringstream temp_ss(lines[1]);
+    std::string temp_str;
+    
+    int num_points;
+    getline(temp_ss,temp_str,' ');
+    num_points = atoi(temp_str.c_str());
+    
+    std::vector<std::string>::iterator points_start = lines.begin() + 2;
+    std::vector<std::string>::iterator points_end = lines.begin() + 2 + num_points;
+    std::vector<Vector3D> points;
+    
+    for(std::vector<std::string>::iterator point = points_start; point != points_end; point++) {
+        float coords[3];
+        std::stringstream point_ss(point->c_str());
+
+        for(int i = 0; i < 3; i++) {
+            getline(point_ss,temp_str,' ');
+            coords[i] = atof(temp_str.c_str());
+        } 
+
+        points.push_back(Vector3D(coords[0],coords[1],coords[2]));
+    } 
+
+    std::vector<Triangle> tris;
+
+    for(std::vector<std::string>::iterator poly = points_end; poly != lines.end(); poly++) {
+        int verts[4];
+        std::stringstream poly_ss(poly->c_str());
+        
+        getline(poly_ss,temp_str,' ');
+        int vert_count = atoi(temp_str.c_str());
+        if (vert_count > 4) {
+            std::cerr << "Error, currently only meshes with tris and quads are supported. Please convert your mesh first" << std::endl;
+        }
+        
+        for (int i = 0; i < vert_count; i++)
+        {
+            getline(poly_ss,temp_str,' ');
+            verts[i] = atoi(temp_str.c_str());
+        }
+        
+        switch (vert_count)
+        {
+        case 3:
+            tris.push_back(Triangle(points[verts[0]], points[verts[1]], points[verts[2]]));
+            break;
+        case 4:
+            // convert quad to tris
+            Triangle* pair = quadToTri(points[verts[0]], points[verts[1]], points[verts[2]], points[verts[3]]);
+            tris.push_back(pair[0]);
+            tris.push_back(pair[1]);
+            // I think I need this? I should prob just use smart pointers instead of raw pointers like a C caveman
+            // this is C++, we have the technology
+            free(pair);
+            break;
+        }
+    }
+
+
+
+}
+
+
+
+float RayIntersectsTriangle(Vector3D rayOrigin, 
                            Vector3D rayVector, 
                            Triangle* inTriangle,
                            Vector3D& outIntersectionPoint)
@@ -77,31 +185,75 @@ bool RayIntersectsTriangle(Vector3D rayOrigin,
     h = rayVector.crossProduct(edge2);
     a = edge1.dotProduct(h);
     if (a > -EPSILON && a < EPSILON)
-        return false;    // This ray is parallel to this triangle.
+        return -1;    // This ray is parallel to this triangle.
     f = 1.0/a;
     s = rayOrigin - vertex0;
     u = f * s.dotProduct(h);
     if (u < 0.0 || u > 1.0)
-        return false;
+        return -1;
     q = s.crossProduct(edge1);
     v = f * rayVector.dotProduct(q);
     if (v < 0.0 || u + v > 1.0)
-        return false;
+        return -1;
     // At this stage we can compute t to find out where the intersection point is on the line.
     float t = f * edge2.dotProduct(q);
     if (t > EPSILON) // ray intersection
     {
         outIntersectionPoint = rayOrigin + rayVector * t;
-        return true;
+        return t;
     }
     else // This means that there is a line intersection but not a ray intersection.
-        return false;
+        return -1;
 }
+
+/* 
+    basic 1 sample 0 bounce tracer
+    trace_rays:
+        for ray in rays:
+            if any(rayTriangleIntersection(ray,tri) for tri in tris):
+                draw white
+            else:
+                draw black
+    
+    intermediate s sample b bounce diffuse tracer
+    trace_rays:
+        for ray in rays:
+            repeat s times:
+            intersections = [rayTriangleIntersection(ray,tri) for tri in tris if t > 0]
+            if no intersections:
+                draw sky/bg color
+            else:
+                if b = 0:
+                    draw color of min(t of intersections)
+                else:
+                    new_ray = min(t of intersections), reflect ray over normal of tri (or random dir for diffuse)
+                    trace_rays(r,tris,s,b-1)
+            
+
+*/
 
 int main(int argc, char const *argv[])
 {
-    /* 
-        some driver code I'm too tired I'll do it tomorrow
-    */
+    Triangle t1;
+    t1.vertex0 = Vector3D(0,0,1);
+    t1.vertex1 = Vector3D(0,1,0);
+    t1.vertex2 = Vector3D(1,0,0);
+
+    Vector3D rayOrigin(0,0,0);
+    Vector3D rayVector(1,1,1);
+    Vector3D intersectionPoint;
+
+    std::cout << "Ray Origin: " << rayOrigin.toString() << std::endl;
+    std::cout << "Ray Direction: " << rayVector.toString() << std::endl;
+    
+    std::cout << "Triangle:" << std::endl;
+    std::cout << "V1: " << t1.vertex0.toString() << std::endl;
+    std::cout << "V2: " << t1.vertex1.toString() << std::endl;
+    std::cout << "V2: " << t1.vertex2.toString() << std::endl;
+
+    bool intersection = RayIntersectsTriangle(rayOrigin,rayVector,&t1,intersectionPoint);
+
+    std::cout << "Intersection: " << intersection << " at " << intersectionPoint.toString() << std::endl;
+
     return 0;
 }
